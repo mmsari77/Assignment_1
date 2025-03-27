170211a1a87fb713e264b86016e3f772f76de549
Sariyev Maxot <aleksandrlabincev@gmail.com>
2025-03-27 22:31:56 +0500

    Task1: done

--- src/Task1/Task1.java
+++ src/Task1/Task1.java
@@ -1,4 +1,19 @@
 package Task1;
 
 public class Task1 {
+    /**
+     * Finds the minimum element in an array using iteration.
+     * @param array The input array of integers.
+     * @return The minimum element in the array.
+     * Time complexity: O(n), where n is the length of the array.
+     */
+    public static int solve(int[] array) {
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
 }
--- test/Task1/Task1Test.java
+++ test/Task1/Task1Test.java
@@ -1,4 +1,30 @@
 package Task1;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class Task1Test {
+    @Test
+    public void testSolveWithPositiveNumbers() {
+        int[] array = {5, 2, 8, 1, 9};
+        assertEquals(1, Task1.solve(array), "Should return the minimum element");
+    }
+
+    @Test
+    public void testSolveWithNegativeNumbers() {
+        int[] array = {-3, -1, -5, -2};
+        assertEquals(-5, Task1.solve(array), "Should return the minimum element");
+    }
+
+    @Test
+    public void testSolveWithSingleElement() {
+        int[] array = {42};
+        assertEquals(42, Task1.solve(array), "Should return the only element");
+    }
+
+    @Test
+    public void testSolveWithExampleFromProblem() {
+        int[] array = {10, 1, 32, 3, 45};
+        assertEquals(1, Task1.solve(array), "Should return the minimum element from problem example");
+    }
 }
